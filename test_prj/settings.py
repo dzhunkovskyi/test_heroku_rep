8e21cad9b5d4c0de3f62ad2c105e1cc3eefe1ba1
TestAccount099 <valera-jonny@ukr.net>
2017-11-18 14:52:00 +0200

    changed settings

--- test_prj/settings.py
+++ test_prj/settings.py
@@ -135,7 +135,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    ('static', 'https://guarded-ravine-82442.herokuapp.com/app/test_prj/static'),
+    ('static', 'app/test_prj/static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
