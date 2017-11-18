4c51eef0dcab9354660110c5f21c47c09aa46247
TestAccount099 <valera-jonny@ukr.net>
2017-11-18 15:09:15 +0200

    settings changed

--- test_prj/settings.py
+++ test_prj/settings.py
@@ -135,7 +135,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'test_prj/static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
