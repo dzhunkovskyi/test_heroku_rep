8fe57a5a0938dfa12dfc0f03cce3dd9c7aa8f1bd
TestAccount099 <valera-jonny@ukr.net>
2017-11-19 16:16:19 +0200

    add email verification signup login logout functionality

--- posts/urls.py
+++ posts/urls.py
@@ -6,4 +6,9 @@ app_name = 'posts'
 
 urlpatterns = [
     url(r'^$', views.main, name='main'),
+    url(r'^login/', views.login_func, name='login_func'),
+    url(r'^logout/', views.logout, name='logout'),
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate, name='activate'),
 ]
