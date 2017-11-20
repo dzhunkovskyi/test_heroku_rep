093455c26e6505ed2aab0598d77214e707381c90
TestAccount099 <valera-jonny@ukr.net>
2017-11-20 09:56:07 +0200

    redirect to main page

--- posts/views.py
+++ posts/views.py
@@ -74,19 +74,20 @@ def signup(request):
 
 
 def activate(request, uidb64, token):
-    try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
-        user = User.objects.get(pk=uid)
-    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
-        user = None
-    if user is not None and account_activation_token.check_token(user, token):
-        user.is_active = True
-        user.save()
-        login(request, user)
-        # return redirect('home')
-        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
-    else:
-        return HttpResponse('Activation link is invalid!')
+	try:
+		uid = force_text(urlsafe_base64_decode(uidb64))
+		user = User.objects.get(pk=uid)
+	except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+		user = None
+	if user is not None and account_activation_token.check_token(user, token):
+		user.is_active = True
+		user.save()
+		login(request, user)
+		# return redirect('home')
+		# return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+		return redirect('/')
+	else:
+		return HttpResponse('Activation link is invalid!')
 
 
 def login_func(request):
